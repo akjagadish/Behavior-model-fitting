d61cf45236ad9017173fcac986ad1ba584e13b5a
akjagadish <akshaykjagadish@gmail.com>
2021-04-26 17:01:19 +0200

    add helper

--- behavior_model_fitting/helpers/policy.py
+++ behavior_model_fitting/helpers/policy.py
@@ -1,3 +1,5 @@
+import torch
+
 def softmax(z, beta):
     """helper function, softmax with beta
 
@@ -16,3 +18,18 @@ def softmax(z, beta):
     """
     assert beta > 0
     return torch.nn.functional.softmax(torch.squeeze(z / beta), dim=0)
+
+def pick_action(action_distribution):
+        """action selection by sampling from a multinomial.
+        Parameters
+        ----------
+        action_distribution : 1d torch.tensor
+            action distribution, pi(a|s)
+        Returns
+        -------
+        torch.tensor(int), torch.tensor(float)
+            sampled action, log_prob(sampled action)
+        """
+        m = torch.distributions.Categorical(action_distribution)
+        a_t = m.sample()
+        return a_t
---
+++ behavior_model_fitting/helpers/utils.py
@@ -0,0 +1,11 @@
+import torch
+import numpy as np
+
+def set_random_seed(seed):
+    """
+    Sets all random seeds
+    """
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
